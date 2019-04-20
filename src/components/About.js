9437dbf9f29cf994d9dc4e401486de8aba3f76fe
Matthew Lam <matthewlam@Matthews-MacBook-Pro.local>
2019-04-20 13:55:00 -0400

    Push project

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum nobis ab velit ducimus inventore. Placeat velit tempore necessitatibus voluptate iusto! Earum sunt dolor accusantium aperiam voluptates explicabo at sint enim.</p>
+        </div>
+    )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = (props) => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum nobis ab velit ducimus inventore. Placeat velit tempore necessitatibus voluptate iusto! Earum sunt dolor accusantium aperiam voluptates explicabo at sint enim.</p>
+        </div>
+    )
+}
+
+export default Contact
